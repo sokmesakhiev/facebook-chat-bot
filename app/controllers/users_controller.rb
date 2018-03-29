c52fa825a7a60e29ca020e7cb4d6152ae238bdf2
Sokly <hengsokly23@gmail.com>
2018-03-29 11:18:05 +0700

    add form user for new and edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,13 +26,12 @@ class UsersController < ApplicationController
   end
 
   def update
-    params = convert_role_params
     @user = User.find(params[:id])
 
     authorize @user
 
-    if @user.update_attributes!(filter_params)
-      redirect_to accounts_path, notice: 'Account has been updated successfully'
+    if @user.update_attributes!(data_params)
+      redirect_to users_path, notice: 'User has been updated successfully'
     else
       flash.now[:alert] = 'Failed to update user'
       render :new
@@ -40,23 +39,19 @@ class UsersController < ApplicationController
   end
 
   def create
-    params = convert_role_params
-    @user = User.new(filter_params)
-
+    @user = User.new(data_params)
     authorize @user
 
-    @User.password = params[:account][:password]
-    @User.password_confirmation = params[:account][:password_confirmation]
-    if @User.valid?
-      if @User.save!
-        redirect_to accounts_path, notice: 'Account has been created successfully'
-      else
-        flash.now[:alert] = 'Failed to save user'
-        render :new
-      end
+    if !@user.valid?
+      flash.now[:alert] = 'Failed to save user'
+      render :new, errors: @user.errors
+    end
+
+    if @user.save!
+      redirect_to users_path, notice: 'User has been created successfully'
     else
       flash.now[:alert] = 'Failed to save user'
-      render :new, errors: @User.errors
+      render :new
     end
   end
 
@@ -65,28 +60,16 @@ class UsersController < ApplicationController
 
     authorize @user
 
-    if @User.destroy!
-      redirect_to accounts_path, notice: 'Account has been deleted'
+    if @user.destroy!
+      redirect_to users_path, notice: 'User has been deleted'
     else
-      redirect_to accounts_path, notice: 'Could not delete account user'
+      redirect_to users_path, notice: 'Could not delete user'
     end
   end
 
   private
 
-  def filter_params
-    params.require(:account).permit(:email, :password, :password_confirmation, :is_admin, :is_counsellor, schools: [])
-  end
-
-  def convert_role_params
-    if params[:account][:role] == Account::ROLE[0]
-      params[:account][:is_admin] = true
-      params[:account][:schools] = nil
-    else
-      params[:account][:is_counsellor] = true
-      params[:account][:schools] = [params[:account][:schools]]
-    end
-    params[:account].delete(:role)
-    params
+  def data_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :role)
   end
 end
