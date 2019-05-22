340354d0031343c3f71ac9a63514dccc3d5b1b6c
thyda_eng <thyda@instedd.org>
2019-05-22 16:34:14 +0700

    remove comment

--- app/helpers/bot_helper.rb
+++ app/helpers/bot_helper.rb
@@ -1,6 +1,5 @@
 module BotHelper
   def survey_control(question)
-    # question = question.becomes(Questions::CarouselQuestion) if question.media_image?
     content_tag :div, class: 'form-group' do
       question.html_template.html_safe
     end
